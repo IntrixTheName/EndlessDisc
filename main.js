629eb3387d073bcf903ed7a5c22c1b955e78e964
Bradley Walby <intrixthename@gmail.com>
2023-09-07 16:38:36 -0400

    User selection prototype

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (request, response){
-    fs.readFile('sample-website.html', function(err, data){
+    fs.readFile('user-selection.html', function(err, data){
         response.writeHead(200,{'Content-Type':'text/html'});
         response.write(data);
         return response.end();
