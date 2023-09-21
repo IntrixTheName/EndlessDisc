893b5a51c7bc1e867d7b420ee603f6f6c867b524
IntrixTheName <intrixthename@gmail.com>
2023-09-20 22:35:26 -0400

    Commenting

--- web_server_link.js
+++ web_server_link.js
@@ -2,6 +2,7 @@
 //const { json } = require("express");
 
 //Leaving this function in for debugging, but not used anymore
+//Tests if web server can recieve a response from database_connection.js
 function TestConnection() {
     fetch("//localhost:2492/connection-test")
     .then((response) => {
@@ -17,24 +18,25 @@ function TestConnection() {
     .catch((error) => console.log('trouble with '))
 }
 
+//Gets all users from the users table and populates dropdown menu on login screen
 function Database_GetUsers() {
     console.log("reading users from database");
     fetch("//localhost:2492/get-users")
     .then((response) => {
         if(!response.ok) {
-            throw new Error("HTTP error");
+            throw new Error("HTTP error ${response.status}");
         }
         return response.text();
     })
     .then((result) => {
         console.log("Query results, users: " + result);
-        let names = JSON.parse(result);
+        let names = JSON.parse(result); //Express returns a string-serialized version, so JSON.parse converts it back into a JSON object
         for(let i in names) {
             console.log(names[i]);
-            let element = document.createElement("option");
-            element.textContent = names[i].name;
-            element.value = names[i].name;
-            document.getElementById("username").appendChild(element);
+            let element = document.createElement("option"); //Create <option></option>
+            element.textContent = names[i].name; //<option>names[i]</option>   Visible text
+            element.value = names[i].name; //<option value="names[i]">names[i]</option>   For identifying individual elements later
+            document.getElementById("username").appendChild(element); //Push to HTML file
         }
     })
 }
